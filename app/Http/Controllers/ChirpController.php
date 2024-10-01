f1543ef0c4da97b68b56824d9025b69146acb24e
John Carlo Yhapon <yhaponjc@gmail.com>
2024-10-01 20:34:54 +0800

    Adjusted the controller

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -1,18 +1,20 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response; 
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response 
     {
-        //
+        return response('Hello, World!');
     }
 
     /**
